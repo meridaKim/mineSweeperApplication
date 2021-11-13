c35e76231a175aff6b7e4d9666072bcd5247101e
meridaKim <71478325+meridaKim@users.noreply.github.com>
2021-11-13 20:50:13 +0900

    button add array2

--- app/src/main/java/com/example/minesweeperapplication/MainActivity.java
+++ app/src/main/java/com/example/minesweeperapplication/MainActivity.java
@@ -14,29 +14,31 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        // setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_main);
         final TableLayout tableLayout = (TableLayout) findViewById(R.id.table);
         tableLayout.setShrinkAllColumns(true);
 
         for (int i = 0; i < 9; i++) {
             // Creation row
-            final TableRow tableRow = new TableRow(this);
+            final TableRow tableRow[] = new TableRow[i];
+                    tableRow[i] = new TableRow(this);
+                    for (int j = 0; j < 9; j++) {
+                        final Button tb [][]= new Button[i][j];
+                        tb [i][j] = new Button(this);
+                        tb[i][j].setText("");
+                        tb[i][j].setLayoutParams
+                                (new TableRow.LayoutParams(TableRow.LayoutParams.WRAP_CONTENT, TableRow.LayoutParams.WRAP_CONTENT));
 
-            for (int j = 0; j < 9; j++) {
-                final Button tb = new Button(this);
-                tb.setText("");
-                tb.setLayoutParams(new TableRow.LayoutParams(TableRow.LayoutParams.WRAP_CONTENT, TableRow.LayoutParams.WRAP_CONTENT));
-
-
-                tableRow.addView(tb);
+                        tableRow[i].addView(tb[i][j]);
+                }
+            tableRow[i].setLayoutParams(new TableLayout.LayoutParams
+                    (TableLayout.LayoutParams.WRAP_CONTENT, TableLayout.LayoutParams.WRAP_CONTENT));
+            tableLayout.addView(tableRow[i]);
             }
 
-            tableRow.setLayoutParams(new TableLayout.LayoutParams(TableLayout.LayoutParams.WRAP_CONTENT, TableLayout.LayoutParams.WRAP_CONTENT));
-
-            tableLayout.addView(tableRow);
         }
     }
-}
+
 
 
 
