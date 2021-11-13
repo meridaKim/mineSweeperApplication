3c1455c64b97472f1292d36dfd01dad71898c795
meridaKim <71478325+meridaKim@users.noreply.github.com>
2021-11-13 20:12:55 +0900

    button array

--- app/src/main/java/com/example/minesweeperapplication/MainActivity.java
+++ app/src/main/java/com/example/minesweeperapplication/MainActivity.java
@@ -2,11 +2,10 @@ package com.example.minesweeperapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.graphics.Color;
+
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
-import android.widget.LinearLayout;
 import android.widget.TableLayout;
 import android.widget.TableRow;
 
@@ -15,42 +14,38 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-       // setContentView(R.layout.activity_main);
+        // setContentView(R.layout.activity_main);
+        final TableLayout tableLayout = (TableLayout) findViewById(R.id.table);
+        tableLayout.setShrinkAllColumns(true);
 
-        LinearLayout l = new LinearLayout(this);
-        l.setOrientation(LinearLayout.VERTICAL);
+        for (int i = 0; i < 9; i++) {
+            // Creation row
+            final TableRow tableRow = new TableRow(this);
 
-        TableLayout table;
-        table = (TableLayout) findViewById(R.id.tableLayout);
+            for (int j = 0; j < 9; j++) {
+                final Button tb = new Button(this);
+                tb.setText("");
+                tb.setLayoutParams(new TableRow.LayoutParams(TableRow.LayoutParams.WRAP_CONTENT, TableRow.LayoutParams.WRAP_CONTENT));
 
-        TableRow tableRow[] = new TableRow[9];
-        TableRow.LayoutParams layoutParams = new TableRow.LayoutParams(
-                TableRow.LayoutParams.WRAP_CONTENT,
-                TableRow.LayoutParams.WRAP_CONTENT, 1.0f);
 
-        setContentView(l, layoutParams);
-
-        for (int k = 0; k < 9; k++) {
-            tableRow[k] = new TableRow(this);
-            tableRow[k].setLayoutParams(layoutParams);
-
-            Button[][] buttons = new Button[9][9];
-            for (int i = 0; i < 9; i++) {
-                for (int j = 0; j < 9; j++) {
-                    buttons[i][j] = new Button(this);
-                    buttons[i][j].setLayoutParams(layoutParams);
-                    buttons[i][j].setBackgroundColor(Color.GRAY);
-                    tableRow[k].addView(buttons[i][j]);
-                    l.addView(buttons[i][j]);
-
-                }
-
+                tableRow.addView(tb);
             }
 
+            tableRow.setLayoutParams(new TableLayout.LayoutParams(TableLayout.LayoutParams.WRAP_CONTENT, TableLayout.LayoutParams.WRAP_CONTENT));
 
+            tableLayout.addView(tableRow);
         }
-        table.addView(table, layoutParams);
-
     }
-
 }
+
+
+
+
+
+
+
+
+
+
+
+
