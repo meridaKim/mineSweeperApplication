a2452932b8718be3678e8e831317be41d330532c
meridaKim <71478325+meridaKim@users.noreply.github.com>
2021-11-13 17:32:15 +0900

    UI design

--- app/src/main/java/com/example/minesweeperapplication/MainActivity.java
+++ app/src/main/java/com/example/minesweeperapplication/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.minesweeperapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.Color;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -14,30 +15,40 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        //setContentView(R.layout.activity_main);
+       // setContentView(R.layout.activity_main);
+
         LinearLayout l = new LinearLayout(this);
         l.setOrientation(LinearLayout.VERTICAL);
-        TableRow.LayoutParams layoutParams = new TableRow.LayoutParams(
-                  TableRow.LayoutParams.WRAP_CONTENT,
-                TableRow.LayoutParams.WRAP_CONTENT, 1.0f);
+        TableLayout table;
+        table = (TableLayout) findViewById(R.id.tableLayout);
 
-        Button[][] buttons = new Button[9][9];
-            for(int i = 0; i < 9; i++){
-                for(int j = 0; j < 9; j++) {
+        TableRow tableRow[] = new TableRow[9];
+        TableRow.LayoutParams layoutParams = new TableRow.LayoutParams(
+                TableRow.LayoutParams.WRAP_CONTENT,
+                TableRow.LayoutParams.WRAP_CONTENT, 1.0f);
+        setContentView(l, layoutParams);
+
+        tableRow[9] = findViewById(R.id.tableRow);
+        for (int k = 0; k < 9; k++) {
+            tableRow[k] = new TableRow(this);
+            tableRow[k].setLayoutParams(layoutParams);
+
+            Button[][] buttons = new Button[9][9];
+            for (int i = 0; i < 9; i++) {
+                for (int j = 0; j < 9; j++) {
                     buttons[i][j] = new Button(this);
                     buttons[i][j].setLayoutParams(layoutParams);
+                    buttons[i][j].setBackgroundColor(Color.GRAY);
+                    tableRow[k].addView(buttons[i][j]);
 
                 }
 
             }
-        TableLayout table;
-        table = (TableLayout)findViewById(R.id.tableLayout);
-        TableRow tableRow = new TableRow(this);
-        table.addView(tableRow);
+
+
+        }
+        table.addView(table, layoutParams);
 
     }
-
-
-
 
 }
